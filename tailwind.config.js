e511cea77a6d01863734aab0a07320ed9ee2ca95
alvinyogakesuma <alvinyoga33@gmail.com>
2023-10-14 04:24:39 +0700

    config tailwind & template (public/client/html)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,28 @@ module.exports = {
       "./resources/**/*.blade.php",
       "./resources/**/*.js",
       "./resources/**/*.vue",
+      "./public/client/html/*.html",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          main: {
+            50: "#FFF9F5",
+            100: "#FBB03B",
+            200: "#FF7A17",
+            300: "#F88312",
+            400: "#0B1742",
+          },
+          pink: "#FB43FF",
+          purple: "#8512F8",
+          white: "#FFFDF6",
+          'white-2': "rgba(193, 113, 39, 0.1)",
+        },
+        fontFamily: {
+          montserrat: ["Montserrat", 'sans-serif'],
+          poppins: ["Poppins", 'sans-serif'],
+        },
+      },
     },
     plugins: [],
   }
