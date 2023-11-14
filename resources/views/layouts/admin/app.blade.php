7eebca588e90d621c179b7b9b9dbf6edbc4957d0
RivaldiYN <aldinggln9@gmail.com>
2023-11-15 05:37:12 +0700

    css update 2

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pemira | @yield('title')</title>
-    @vite('resources/css/app.css')
-
+    {{-- @vite('resources/css/app.css') --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
 
 
     <link rel="shortcut icon" href="{{ asset('mazzer/compiled/svg/favicon.svg') }}" type="image/x-icon">
