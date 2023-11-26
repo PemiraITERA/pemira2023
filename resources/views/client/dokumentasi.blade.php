9a44fb41813fc99ba3ea33f39d36f10c753dddcc
alvinyogakesuma <alvinyoga33@gmail.com>
2023-11-26 19:54:17 +0700

    update sizing dokumentasi

--- resources/views/client/dokumentasi.blade.php
+++ resources/views/client/dokumentasi.blade.php
@@ -20,9 +20,7 @@
                 <div
                     class="bg-green-400 w-full h-56 max-sm:h-40 max-lg:h-44"
                 >
-                    <div class="">
-                        <img src="{{ asset('storage/dokumentasi/'.$item->foto) }}" alt="dokumentasi" />
-                    </div>
+                    <img src="{{ asset('storage/dokumentasi/'.$item->foto) }}" alt="dokumentasi" class="object-cover w-full h-full"/>
                 </div>
             @endforeach
         </div>
